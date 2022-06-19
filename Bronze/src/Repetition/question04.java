1dba65d2a31480cd7801df18006bb8fd0ed92385
DeveloperDulli <dulli1616@kakao.com>
2022-06-19 23:19:38 +0900

    Bronze

---
+++ Bronze/src/Repetition/question04.java
@@ -0,0 +1,18 @@
+package Repetition;
+
+import java.util.Scanner;
+
+public class question04 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		
+		for(int i = 1; i <= a; i++) {
+			
+			System.out.println(i);
+		}
+	}
+
+}
