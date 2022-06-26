619a991877f79cf161aa09b893650f2b1defa529
DeveloperDulli <dulli1616@kakao.com>
2022-06-26 14:27:21 +0900

    2022.06.26

---
+++ Bronze/src/Repetition/question08.java
@@ -0,0 +1,20 @@
+package Repetition;
+
+import java.util.Scanner;
+
+public class question08 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int count = sc.nextInt();
+		
+		for(int i = 1; i <= count; i++) {
+			for(int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+			
+		}
+	}
+}	
