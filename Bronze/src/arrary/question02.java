3c81d80cb430609f1aeac69c231b20474892f5aa
DeveloperDulli <dulli1616@kakao.com>
2022-09-04 15:24:49 +0900

    Array

---
+++ Bronze/src/arrary/question02.java
@@ -0,0 +1,29 @@
+package arrary;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class question02 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] arr = new int[9];
+		int max = 0;
+		int index = 0;
+		
+		for(int i = 0; i < 9; i++) {
+			arr[i] = sc.nextInt();
+		}
+		for(int i = 0; i < 9; i++) {
+			if(arr[i] > max) {
+				max = arr[i];
+				index = i+1;
+			}
+		}
+
+		System.out.println(max);
+		System.out.println(index);
+	}
+
+}
