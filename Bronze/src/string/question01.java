afad1d832c325909615ce35794c85ede531c0a6d
DeveloperDulli <dulli1616@kakao.com>
2022-09-16 18:19:47 +0900

    String

---
+++ Bronze/src/string/question01.java
@@ -0,0 +1,14 @@
+package string;
+
+import java.util.Scanner;
+
+public class question01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.next().charAt(0);
+		System.out.println(A);
+	}
+
+}
